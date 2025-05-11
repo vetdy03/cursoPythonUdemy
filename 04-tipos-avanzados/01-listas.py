5ac6655e1d82f2c93da310a70b0e466874010f9f
vetdy03 <cocaever0@gmail.com>
2025-05-11 10:14:21 -0400

    agrenado el avance del curso de python en udemy el tipos de datos avanzados

---
+++ 04-tipos-avanzados/01-listas.py
@@ -0,0 +1,20 @@
+numeros = [1, 2, 3, 4]
+for id in numeros:
+    print(id)
+
+letras = ["a", "b"]
+palabras_felices=["olaf", "xd", "mundo"]
+boolena = [True, False]
+matriz =[[0,1], [1,0], [0,3]]
+ceros = [0, 1]*10
+alfanumerico = numeros + letras
+rago = list(range(1,11))
+chars = list("hola mundo")
+print(chars)
+print(rago)
+
+print(alfanumerico)
+print(ceros)
+print(matriz)
+print(numeros)
+print(boolena)
