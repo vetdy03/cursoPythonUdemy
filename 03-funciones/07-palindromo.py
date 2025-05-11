903a66e456eb400c502c23272b9d00bd89657393
vetdy03 <cocaever0@gmail.com>
2025-05-11 18:04:03 -0400

    agrenado el avance del curso de python en udemy 01 y 03

--- 03-funciones/07-palindromo.py
+++ 03-funciones/07-palindromo.py
@@ -1,4 +1,21 @@
-def es_palindromo(text):
-    pass
-
+def es_palindromo():
+    palabra = input("Ingrese palabra:")
+    palabra = no_space(palabra)
+    print(palabra)
+    if palabra == (palabra[::-1]):
+        print("Es palindromo")
+    else:
+        print("No es palidromo")
+        
+def no_space(pala):
+    nuevo_text = ""
+    for char in pala:
+        if char != " ":
+            nuevo_text += char
+    
+    return nuevo_text
+            
+    
+if __name__ == "__main__":
+    es_palindromo()
 #pistas eliminar espacios y volvtear palabra
