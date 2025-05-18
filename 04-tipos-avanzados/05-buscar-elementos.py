0259e5c06272e1de8f698b1b24e228ff5a655eb3
vetdy03 <cocaever0@gmail.com>
2025-05-18 12:31:17 -0400

    buscando elementos en listas con el count estab super *****

---
+++ 04-tipos-avanzados/05-buscar-elementos.py
@@ -0,0 +1,10 @@
+mascotas = ["migui", "negro", "migui","pato", "migui", "olaf", "pelusa"]
+print(mascotas.count("migui"))
+if "pepe" in mascotas:
+    print(mascotas.index("negro")) # lo busca por ti y te dice enque posicion esta ese elemento
+ 
+ 
+ 
+    
+#metodo count: CUENTA POR TI CUANTOS VECES APAREWCE EL MISMO ELEMENTO
+#QUE LE PASASTE POR PARAMETRO; SINTAXIS--> mascotas.count("migui")sdñV
