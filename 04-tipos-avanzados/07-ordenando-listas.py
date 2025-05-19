8c50033f76a9408db0da6754d4d261d8f40f214d
vetdy03 <cocaever0@gmail.com>
2025-05-18 23:08:25 -0400

    metodo lambda y compresion de listas*

--- 04-tipos-avanzados/07-ordenando-listas.py
+++ 04-tipos-avanzados/07-ordenando-listas.py
@@ -7,12 +7,22 @@ numeros.sort(reverse=True)#de atras adela
 print(numeros2)
 print(numeros)
 
-users = [[4, "chanchito"],
-         [1, "felipe"],
-         [5, "pulga"]
+# users = [[4, "chanchito"],
+#          [1, "felipe"],
+#          [5, "pulga"]
+#          ]
+users = [["chanchito", 8],
+         ["zelipe", 7],
+         ["pulga", 4]
          ]
 
 def ordena(ele):
-    return ele[]
-users.sort()
+    return ele[1]
+olaf = ordena(users)
+print(olaf)
+
+users.sort(key=ordena)#orden segun el ordena
 print(users)
+users.sort(key=ordena, reverse= True) 
+print(users)
+
