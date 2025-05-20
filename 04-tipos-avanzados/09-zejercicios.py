560da8cca0d74b805663e72e9c559e659158e349
vetdy03 <cocaever0@gmail.com>
2025-05-20 01:44:55 -0400

    zejercicios mejorando

--- 04-tipos-avanzados/09-zejercicios.py
+++ 04-tipos-avanzados/09-zejercicios.py
@@ -6,16 +6,21 @@ listaDeCompras = [["cebolla",10],
                   ["arroz",480],
                   ["azucar",350],
                   ["aceite",22],
-                  ["naranja", 22]
+                  ["naranja",22]
                   ]
 #DESCUENTO DEL 50 A PRODUCTOS QUE VALEN MAS DE 50
 for producto in listaDeCompras:
-    if producto[1] > 50:
-        
-        listaDeCompras[producto[1]//2]
-        print(producto[1])
-        
+    if producto[1] < 25:
+        listaDeCompras.remove(producto)
+        #print(producto[1])
+    elif producto[1] > 50:
+        producto[1] = int(producto[1]*0.5)
+
+print(listaDeCompras)
+
+
+#ELIMINAR PRODUCTOS QUE VALEN MENOS DE 25
+
 #listaDeCompras = list(map(lambda listaNueva: listaNueva[0]  ))
-#print(listaDeCompras)
 
 #NO SE PUEDE ->listaDeCompras2 = list(filter(lambda listadecomp: ((listadecomp[1] > 50)//2), listaDeCompras ))
