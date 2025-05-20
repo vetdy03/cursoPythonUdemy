81ad162e994a322ecc5bb9b9eab8717817af617e
vetdy03 <cocaever0@gmail.com>
2025-05-20 10:22:19 -0400

    zejercicios mejorando a su ultima version: ELIMINAR PRODUCTOS CON MENOS DE 25 USDT

--- 04-tipos-avanzados/09-zejercicios.py
+++ 04-tipos-avanzados/09-zejercicios.py
@@ -1,4 +1,4 @@
-listaDeCompras = [["cebolla",10],
+listaDeCompras = [["cebolla",10], #1#
                   ["carne",104],
                   ["queso",12],
                   ["pollo",90],
@@ -8,18 +8,27 @@ listaDeCompras = [["cebolla",10],
                   ["aceite",22],
                   ["naranja",22]
                   ]
+
 #DESCUENTO DEL 50 A PRODUCTOS QUE VALEN MAS DE 50
-for producto in listaDeCompras:
-    if producto[1] < 25:
-        listaDeCompras.remove(producto)
-        #print(producto[1])
-    elif producto[1] > 50:
-        producto[1] = int(producto[1]*0.5)
-
-print(listaDeCompras)
-
+def aplicarDescuentoMenos50(listaDeComprass):
+    for producto in listaDeCompras:
+        if producto[1] > 50:
+            producto[1] = int(producto[1]*0.5)
+    
+    return listaDeComprass
+print(aplicarDescuentoMenos50(listaDeCompras))
 
 #ELIMINAR PRODUCTOS QUE VALEN MENOS DE 25
+def eliminarMenosDe10(listaDeComprass):
+    for i in range(len(listaDeComprass)-1, -1, -1):
+        if listaDeComprass[i][1] < 25:
+            del(listaDeComprass[i])
+    return listaDeComprass
+
+print(eliminarMenosDe10(listaDeCompras))
+
+
+
 
 #listaDeCompras = list(map(lambda listaNueva: listaNueva[0]  ))
 
