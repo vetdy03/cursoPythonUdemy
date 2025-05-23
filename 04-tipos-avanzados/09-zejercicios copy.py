e11516caab1d0f58739cd22b063f1decd59cb38a
vetdy03 <cocaever0@gmail.com>
2025-05-22 23:58:05 -0400

    notas de estudiantes

---
+++ 04-tipos-avanzados/09-zejercicios copy.py
@@ -0,0 +1,13 @@
+listaDeCompras = [["cebolla",10], #1#
+                  ["carne",104],
+                  ["queso",12],
+                  ["pollo",90],
+                  ["salsa", 20],
+                  ["papa",200],
+                  ["arroz",480],
+                  ["azucar",350],
+                  ["aceite",22],
+                  ["naranja",22]
+                  ]
+
+#aplicar estudiantes notas
