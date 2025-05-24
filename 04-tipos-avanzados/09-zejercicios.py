22130e850dc201439a69c15f0423ee071c80de53
vetdy03 <cocaever0@gmail.com>
2025-05-24 14:07:04 -0400

    agregando un nueva lista con productos economicos

--- 04-tipos-avanzados/09-zejercicios.py
+++ 04-tipos-avanzados/09-zejercicios.py
@@ -8,24 +8,39 @@ listaDeCompras = [["cebolla",10], #1#
                   ["aceite",22],
                   ["naranja",22]
                   ]
+#FILTRAR PRODUCTOS ECONOMICOS
+proEconomicos = []
+def productosEconomicos(listaDeComprass):
+    for producto in listaDeComprass:
+        if producto[1] < 50:
+            proEconomicos.append(producto)
+    return proEconomicos
+print("\nfiltra productos economicos")
+print(productosEconomicos(listaDeCompras))
 
 #DESCUENTO DEL 50 A PRODUCTOS QUE VALEN MAS DE 50
+#MEJORANDO ESTO Si un producto cuesta más de 100, aplicarle 30%. Si cuesta entre 50 y 100, 15%. Si cuesta menos de 50, no tiene descuento.
 def aplicarDescuentoMenos50(listaDeComprass):
-    for producto in listaDeCompras:
-        if producto[1] > 50:
-            producto[1] = int(producto[1]*0.5)
-    
+    for producto in listaDeComprass: # 
+        if producto[1] > 100:
+            producto[1] = producto[1] - int(producto[1]*0.3)
+        if 100 > producto[1] >= 50:
+            producto[1] = producto[1] - int(producto[1]*0.15)
+            
     return listaDeComprass
+print("\naplicar descuentos de 100 30%......")
 print(aplicarDescuentoMenos50(listaDeCompras))
 
 #ELIMINAR PRODUCTOS QUE VALEN MENOS DE 25
-def eliminarMenosDe10(listaDeComprass):
-    for i in range(len(listaDeComprass)-1, -1, -1):
-        if listaDeComprass[i][1] < 25:
-            del(listaDeComprass[i])
-    return listaDeComprass
+# def eliminarMenosDe10(listaDeComprass):
+#     for i in range(len(listaDeComprass)-1, -1, -1):
+#         if listaDeComprass[i][1] < 25:
+#             del(listaDeComprass[i])
+#     return listaDeComprass
+# print(eliminarMenosDe10(listaDeCompras))
 
-print(eliminarMenosDe10(listaDeCompras))
+
+    
 
 #lumnos con baja nota de 51#
 
