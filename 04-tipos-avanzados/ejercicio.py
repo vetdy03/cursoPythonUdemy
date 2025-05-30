dec3c58015dc6daa2a01eeb5553c68aa5e1d36f8
vetdy03 <cocaever0@gmail.com>
2025-05-29 23:54:45 -0400

    contando en un diccionario los caracteres que se repirtene n un string

--- 04-tipos-avanzados/ejercicio.py
+++ 04-tipos-avanzados/ejercicio.py
@@ -1,8 +1,14 @@
 #eliminar los espacios vacios de un strin y devolver una lista con los caracteres restantes
-cadenita = input("Escribe tu cadenita: ")
+cadenita = input("Escribe tu cadenita: ")#lagunala
 nuevaCadena = ""
 for charr in cadenita:
     if charr != " ":
         nuevaCadena += charr
         
 print("Devolviendo la nueva {}".format(nuevaCadena))
+
+#Contar en un diccionario cuantos se repiten 
+cadenita2 = list(cadenita)
+dic = dict
+for charr in cadenita2:
+    
