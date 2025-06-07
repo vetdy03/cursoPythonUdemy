6b3be22d6822b356a33a603df8d3726b011c10e0
vetdy03 <cocaever0@gmail.com>
2025-06-07 01:25:52 -0400

    tss secei es noche

--- 04-tipos-avanzados/ejercicio.py
+++ 04-tipos-avanzados/ejercicio.py
@@ -1,14 +1,15 @@
 #eliminar los espacios vacios de un strin y devolver una lista con los caracteres restantes
 cadenita = input("Escribe tu cadenita: ")#lagunala
-# nuevaCadena = ""
-# for charr in cadenita:
-#     if charr != " ":
-#         nuevaCadena += charr
+nuevaCadena = ""
+for charr in cadenita:
+    if charr != " ":
+        nuevaCadena += charr
         
-# print("Devolviendo la nueva {}".format(nuevaCadena))
+print("Devolviendo la nueva {}".format(nuevaCadena))
 
 #Contar en un diccionario cuantos se repiten 
-cadenita2 = list(cadenita.lower())#lagunala
+cadenita2 = list(nuevaCadena.lower())#lagunala
+#cadenita2 = list(cadenita.lower())#lagunala
 dic = {}
 for charr in cadenita2:
     if charr not in dic:
@@ -19,6 +20,16 @@ for charr in cadenita2:
 sumaDeCaracteres = sum(dic.values())
 print("Suma de caracteres:", sumaDeCaracteres)
 print(dic)
-## 5 2 3 8 1 9
+## SALIDA: {'d': 3, 'i': 8, 'c': 6, 'o': 7, 'n': 6, 'a': 10, 'r': 5, 'e': 3, 'u': 3, 'q': 1, 'm': 2, 'b': 1, 's': 1, 'p': 2, 'l': 1}
 
 
+#verificar cuáles valores son mayores a 4
+#letra toma el valor de la clave (en tu caso, cada carácter de la cadena).
+#cantidad toma el valor asociado a esa clave (cuántas veces aparece ese carácter).
+for letra, cantidad in dic.items():#devuelve una lista de tuplas, donde cada tupla es (clave, valor) del diccionario.
+    if cantidad > 4:
+        print(f"La letra '{letra}' se repite {cantidad} veces.")
+
+for llave, cant in dic.items():
+    if cant >= 4:
+        print("La letra {} se repite {}".format(llave, cant))
