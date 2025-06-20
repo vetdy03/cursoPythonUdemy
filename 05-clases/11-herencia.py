4238628c4fb339ec055791423f832f77f06e6fd8
vetdy03 <cocaever0@gmail.com>
2025-06-20 11:31:00 -0400

    herencia multi-nivel

--- 05-clases/11-herencia.py
+++ 05-clases/11-herencia.py
@@ -1,13 +1,20 @@
-class Perro:
+class Animal:
+    def comer(self):
+        print("Comiendo")
+        
+class Perro(Animal):
     def pasear(self):
         print("paseando")
-        
-    def comer(self):
-        print("comiendo")
-        
-class Chanchito:
-    def comer (self):
-        print("comiendo")
-        
-    def programar(self):
+
+perro = Perro()
+perro.comer()
+
+class Chanchito(Perro): #herencia multi-nivel no es recomendable mas de 2
+    def programar (self):
         print("Programando")
+        
+chanchi = Chanchito()
+chanchi.comer()
+chanchi.pasear()
+
+#si tendria un metodo en comun tendria qe modficiar en cada clase por eso existe l herencia
