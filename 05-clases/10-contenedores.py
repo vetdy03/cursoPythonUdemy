5e0d5e6a0ea7491b9d08a6c9ac038a885461cc3b
vetdy03 <cocaever0@gmail.com>
2025-06-20 01:43:52 -0400

    mejorando en herencia

--- 05-clases/10-contenedores.py
+++ 05-clases/10-contenedores.py
@@ -7,6 +7,7 @@ class Producto:
     #metodo magico que devuelve el nombr y precio como STRING    
     def __str__(self):
         return f"Producto: {self.nombre} - Precio: {self.precio}"
+    
 class Categoria:
     #atributo que se encarga de contener los productos que querramos
     productos = []
@@ -14,3 +15,18 @@ class Categoria:
     def __init__(self, nombre, productos):
         self.nombre = nombre
         self.productos = productos
+        
+    def agregar(self, producto):
+        self.productos.append(producto)
+        
+    def imprimir(self):
+        for producto in self.productos:
+            print(producto)
+kayak = Producto("kayak", 1000)
+bicicleta = Producto("bici", 5600)
+laptop = Producto("laptop", 500)
+
+deportes = Categoria("deporte", [kayak, bicicleta])
+
+deportes.agregar(laptop)
+deportes.imprimir()
---
+++ 05-clases/11-herencia.py
@@ -0,0 +1,13 @@
+class Perro:
+    def pasear(self):
+        print("paseando")
+        
+    def comer(self):
+        print("comiendo")
+        
+class Chanchito:
+    def comer (self):
+        print("comiendo")
+        
+    def programar(self):
+        print("Programando")
