d6b6b16668e0c6a5c062e2a85121db67a2bcbe36
vetdy03 <cocaever0@gmail.com>
2025-07-04 23:19:43 -0400

    else e finally

---
+++ 06-excepciones/03-else-finally.py
@@ -0,0 +1,9 @@
+try:
+    n1 = int(input("Introduce un primer número: "))
+except ValueError as e:
+    print("Ingrese un valor que corresponda.")
+else:
+    print("no hubo errores)")
+finally:
+    print("Esto se ejecuta siempre, independientemente de si hubo una excepción o no.")
+    print("Fin del programa.")
