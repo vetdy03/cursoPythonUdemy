9b5c473b4a7fc61280e2b13f0c9da8b41674f1dc
vetdy03 <cocaever0@gmail.com>
2025-07-04 23:10:12 -0400

    excepciones manejando 02

---
+++ 06-excepciones/01-intro.py
@@ -0,0 +1,8 @@
+# print("Holi")
+# lista = [1, 2, 3, 4, 5]
+# lista[10]  # Esto generará un IndexError porque el índice 10 no existe en la lista
+
+try:
+    n1 = int(input("Introduce un primer número: "))
+except:
+    print("No has introducido un número válido.")
---
+++ 06-excepciones/02-tipos-de-excepciones.py
@@ -0,0 +1,7 @@
+try:
+    n1 = int(input("Introduce un primer número: "))
+    adfd
+except ValueError as e:
+    print("Ingrese un valor que corresponda.")
+except NameError as e:
+    print("Ocurrio un error de nombre.")
