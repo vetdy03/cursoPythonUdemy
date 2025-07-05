d2db507d760f24fa64e7eb2ea846fbe38fc37a1e
vetdy03 <cocaever0@gmail.com>
2025-07-04 20:50:53 -0400

    probando ando el delete 2

--- delete.py
+++ delete.py
@@ -1,1 +1,9 @@
-hola mundo 
+hola mundo def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+if __name__ == "__main__":
+    n = int(input("¿Cuántos números de Fibonacci quieres ver? "))
+    fibonacci(n)
