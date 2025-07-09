af4fa7925c2c67c0eb6857c1e75901584adf6804
vetdy03 <cocaever0@gmail.com>
2025-07-08 23:10:03 -0400

    modulos app y usuarios_impuestos

--- 07-modulos/app.py
+++ 07-modulos/app.py
@@ -1,3 +1,8 @@
-from usuario_impuestos import *
+from usuario_impuestos import guardar, pagarImpuestos # * #Nunca importar todo con *
+
+# guardar()
+def guardar():
+    print("soy app.py y estoy guardando datos...")
+
 
 guardar()
