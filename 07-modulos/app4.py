703c621d1ff079aa2d5ae378d1eb1d2e69b9e9ed
vetdy03 <cocaever0@gmail.com>
2025-07-13 19:26:00 -0400

    agregando capitulo de dir

---
+++ 07-modulos/app4.py
@@ -0,0 +1,8 @@
+import usuarios.gestion
+import usuarios.impuestos
+from usuarios.impuestos.utilidades import pagar_impuestos
+import usuarios
+print(usuarios.gestion.__name__)
+print(usuarios.impuestos.__package__)
+print(usuarios.gestion.__path__)
+print(usuarios.gestion.__file__)
