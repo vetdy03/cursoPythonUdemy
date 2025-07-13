3ffba2b32fc41af7ae31ec414036c8d5c408328c
vetdy03 <cocaever0@gmail.com>
2025-07-13 19:35:20 -0400

    paquetes con nombres dinamicos

--- 07-modulos/app4.py
+++ 07-modulos/app4.py
@@ -1,8 +1,10 @@
-import usuarios.gestion
-import usuarios.impuestos
+
 from usuarios.impuestos.utilidades import pagar_impuestos
-import usuarios
-print(usuarios.gestion.__name__)
-print(usuarios.impuestos.__package__)
-print(usuarios.gestion.__path__)
-print(usuarios.gestion.__file__)
+#import usuarios
+#pagar_impuestos()
+print(__name__)
+
+# print(usuarios.gestion.__name__)
+# print(usuarios.impuestos.__package__)
+# print(usuarios.gestion.__path__)
+# print(usuarios.gestion.__file__)
--- 07-modulos/usuarios/impuestos/utilidades.py
+++ 07-modulos/usuarios/impuestos/utilidades.py
@@ -1,5 +1,10 @@
-from ..gestion.crud import guardar_crud 
+#from ..gestion.crud import guardar_crud 
+
+print(__name__)
 
 def pagar_impuestos():
     print("Pagando impuestos desde utilidades.py...")
-    guardar_crud()
+    #guardar_crud()
+
+if __name__ == "__main__":
+    print("tarea de mantenimiento")
