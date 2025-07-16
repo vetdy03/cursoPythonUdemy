c5fc8d845b1cf0574f4d5acbcfdae99f30aa2174
vetdy03 <cocaever0@gmail.com>
2025-07-16 02:29:39 -0400

    entrando a rutas con path

--- 07-modulos/usuarios/impuestos/utilidades.py
+++ 07-modulos/usuarios/impuestos/utilidades.py
@@ -1,11 +1,12 @@
+if __name__ != "__main__":
+    from usuarios.gestion.crud import guardar_crud 
+    from ..gestion.crud import guardar_crud 
+
+    print(__name__)
+
+    def pagar_impuestos():
+        print("Pagando impuestos desde utilidades.py...")
+        guardar_crud()
+
 if __name__ == "__main__":
     print("tarea de mantenimiento")
-
-from ..gestion.crud import guardar_crud 
-
-print(__name__)
-
-def pagar_impuestos():
-    print("Pagando impuestos desde utilidades.py...")
-    guardar_crud()
-
