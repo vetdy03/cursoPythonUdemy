a36261609b55029a12ddc0adcd9369b73f0240df
vetdy03 <cocaever0@gmail.com>
2025-07-20 23:53:36 -0400

    agregando PATH y con inyeccions y deps

--- 08-rutas/03-inyeccion-deps.py
+++ 08-rutas/03-inyeccion-deps.py
@@ -4,5 +4,11 @@
 #     def __init__(self, Correa):#construcctor
 #         self.correa = Correa() #contructor de la clase Correa
 
-def init_app(bbdd, api):
+#def init_app(bbdd, api):
     #iniciando el modulo de rutas
+
+from pathlib import Path # from two import init as init_two
+
+path  = Path()
+paths = [p for p in path.iterdir() if p.is_dir()] #esto es asignacion de lista por comprension
+    paquete = __import__()
---
+++ 08-rutas/two/__init__.py
@@ -0,0 +1,2 @@
+def init():
+    print("Soy modulo dos")
