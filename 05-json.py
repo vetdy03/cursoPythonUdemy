4972b97219249a68f4f648a52685d4ee5964eda9
vetdy03 <cocaever0@gmail.com>
2025-08-13 18:21:00 -0400

    05 esribir, leer y modificar un json. JSON JavaScript Object Notation

--- 05-json.py
+++ 05-json.py
@@ -3,8 +3,21 @@ from pathlib import Path
 
 #escribir
 
-productos = [ 
-    {"id": 1, "name": "surf"},
-    {"id": 2, "name": "skate"},
-    {"id": 3, "name": "bici"}
-]
+# productos = [ 
+#     {"id": 1, "name": "surf"},
+#     {"id": 2, "name": "skate"},
+#     {"id": 3, "name": "bici"}
+# ]
+
+# data = json.dumps(productos)
+# # print(data)  # Imprime el JSON como una cadena
+# Path("09-archivos/productos.json").write_text(data) 
+
+#LEER JSON
+data = Path("09-archivos/productos.json").read_text(encoding="utf-8")  # Lee el contenido del archivo JSON 
+productos = json.loads(data) # Convierte el JSON a un objeto de Python
+print(productos)  # Imprime la lista de productos
+
+#MODIFICAR JSON
+productos[0]["name"] = "feliz"
+Path("09-archivos/productos.json").write_text(json.dumps(productos))  # Escribe la lista de productos modificada de nuevo en el archivo JSON
