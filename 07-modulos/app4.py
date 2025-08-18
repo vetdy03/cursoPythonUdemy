c4cff3901742bf747adb333f20f3696d2a49d9be
vetdy03 <cocaever0@gmail.com>
2025-08-18 11:20:20 -0400

    pip, com instalar y desinstalar diferentes versiones

--- 07-modulos/app4.py
+++ 07-modulos/app4.py
@@ -2,7 +2,7 @@
 from usuarios.impuestos.utilidades import pagar_impuestos
 #import usuarios
 pagar_impuestos()
-print(__name__)
+print(__name__) 
 
 # print(usuarios.gestion.__name__)
 # print(usuarios.impuestos.__package__)
