0cb79e319647b0e6c7f6f84702cb0320b0dc677b
vetdy03 <cocaever0@gmail.com>
2025-09-25 17:25:32 -0400

    paque populares-Requests

--- 13-paquetes-populares/03-requests.py
+++ 13-paquetes-populares/03-requests.py
@@ -1,8 +1,9 @@
 import requests
 
-# url = "https://jsonplaceholder.typicode.com/users"
+#url = "https://jsonplaceholder.typicode.com/users"
 
-# r = requests.get(url, timeout= 10) #get, post, put/path, delete
+#METODO GET
+#r = requests.get(url, timeout= 10)# esto es una peticion http donde url es el recurso y #get, post, put/path, delete
 # print(
 #     r.status_code,
 #     r.text
@@ -10,14 +11,38 @@ import requests
 # r= r.json() #esto se llama casting o parseo o conversion o 
 # for user in r:
 #     print(user["name"],"-", user["email"])
+
+
+##METODO GET PARA LISTAR SOLO UN USER
+# url = "https://jsonplaceholder.typicode.com/users/1"
+# r = requests.get(url, timeout=10)
+# print(r.json())
+
+##METODO POST PARA CREAR UN USER
+# url = "https://jsonplaceholder.typicode.com/users/"
+# user = {
+#     "id": 11,
+#     "name": "chanchito feliz"
+# }
+# r = requests.post(url, timeout=10, data=user) #que hace esta linea de odigo? #hace una peticion post a la url con el user como data que mandamos 
+# print(r.json())
+# print(r.status_code)
+
+##METODO PUT/PATCH PARA ACTUALIZAR UN USER
+# url = "https://jsonplaceholder.typicode.com/users/2"
+# user = {
+#     "name": "chanchito triste"
+# }
+# r = requests.put(url, timeout=10, data=user) 
+# print(r.json())
+# print(r.status_code)
 # 
-url = "https://jsonplaceholder.typicode.com/users/1"
-user = {
-    "id": 11,
-    "name": "chanchito feliz"
-}
-r = requests.post(url, timeout=10, data=user)
 
+##METODO DELETE PARA ACTUALIZAR UN USER
+url = "https://jsonplaceholder.typicode.com/users/2"
+r = requests.put(url, timeout=10) 
+# print(r.json())
+print(r.status_code)
 
-# API = APLICATIONS PROGRAMING INTERFACE
-# rest = REPRESENTATIONAL STATE TRANSFER
+# API = APLICATIONS PROGRAMING INTERFACE= es como tu interactuas con un software 
+# rest = REPRESENTATIONAL STATE TRANSFER = se usa para representar recursos
