61fcee6d22fa862d6f98950b6a4acc97d96d2136
vetdy03 <cocaever0@gmail.com>
2025-09-25 17:32:03 -0400

    paque populares-headers en requests

--- 13-paquetes-populares/03-requests.py
+++ 13-paquetes-populares/03-requests.py
@@ -2,7 +2,7 @@ import requests
 
 #url = "https://jsonplaceholder.typicode.com/users"
 
-#METODO GET
+##METODO GET
 #r = requests.get(url, timeout= 10)# esto es una peticion http donde url es el recurso y #get, post, put/path, delete
 # print(
 #     r.status_code,
@@ -37,12 +37,25 @@ import requests
 # print(r.json())
 # print(r.status_code)
 # 
+# 
+#METODO DELETE PARA ACTUALIZAR UN USER
+# url = "https://jsonplaceholder.typicode.com/users/2"
+# r = requests.put(url, timeout=10) 
+# print(r.json())
+# print(r.status_code)
 
 ##METODO DELETE PARA ACTUALIZAR UN USER
 url = "https://jsonplaceholder.typicode.com/users/2"
-r = requests.put(url, timeout=10) 
+apikeyOfVetdy03 = "1234567890"
+headers = {
+    "Authorization": f"Bearer {apikeyOfVetdy03}"
+}
+r = requests.put(url, timeout=10, headers=headers) 
 # print(r.json())
 print(r.status_code)
+
+
+
 
 # API = APLICATIONS PROGRAMING INTERFACE= es como tu interactuas con un software 
 # rest = REPRESENTATIONAL STATE TRANSFER = se usa para representar recursos
