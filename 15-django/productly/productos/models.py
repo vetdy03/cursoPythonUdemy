7298d7839dafa1059a9f12cf2b503ea34aeb47a7
vetdy03 <cocaever0@gmail.com>
2025-09-29 15:47:20 -0400

    15-django-models

--- 15-django/productly/productos/models.py
+++ 15-django/productly/productos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(nax_length=100) #longuitud maxima 100 que debe guardar
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    #CASCADE: ELIMINAR EL PRODUCTO
+    # PROTECT: LANZA UN ERROR
+    # RESTRICT: SOLO ELIMINA SI NO EXISTEN PRODUCTOS
+    # SET_NULL: ACTUALIZA EL VALOR
+    # SET_DEFAULT: ASIGAN VAL POR DEFECTO
+    categoria = models.ForeignKey(Categoria, on_delete=)
