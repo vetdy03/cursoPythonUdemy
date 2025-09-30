450b2ac11a9cb619eb4c9e16aff260e8400b0a7b
vetdy03 <cocaever0@gmail.com>
2025-09-29 23:51:16 -0400

    15-django- interactuando con los datos

--- 15-django/productly/productos/views.py
+++ 15-django/productly/productos/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 from django.shortcuts import render
+from .models import Producto
+#productos
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola mundo!!')
+    productos = Producto.objects.all().values() # si quetremos ver en un json debemos usar values()
+
+    # print(productos)
+    return JsonResponse(list(productos), safe=False) # si no es un diccionario poner safe en false 
