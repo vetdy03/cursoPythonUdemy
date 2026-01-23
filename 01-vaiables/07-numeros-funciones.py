87815c1c83bb2ed13ab74fbfc12d6370e8ba32f4
vetdy03 <cocaever0@gmail.com>
2026-01-23 13:45:50 -0400

    modificando calse 01 de varibles en python

--- 01-vaiables/07-numeros-funciones.py
+++ 01-vaiables/07-numeros-funciones.py
@@ -1,13 +1,19 @@
 import math
 
-print(round(1.3))
-print(abs(-3))
+print(round(8.33))
+negativo = -4
+if negativo < 0:
+    postivo = negativo * (-1)
+    print(postivo)
+print(abs(-3)) # lo vuelve postivo el numero
+
 
 print(math.ceil(-10)) ##lleva al num entero mas cercano
 print(math.floor(2.99)) #al numero entero hacia abajo
-print(math.isnan(1.2))
-print(int(math.pow(10,2)))
-print(math.sqrt(9))
-print(math.fma(3,3,5))
+print(math.isnan(8)) # esto es para saber si es un numero o no
+print(math.factorial(5)) # saca el factorial de un numero
+print(int(math.pow(10,2))) #Eleva a la potencia
+print(math.sqrt(9)) #eleva al cuadrado
+print(math.fma(3,3,5)) 
 print(math.modf(16))
 print(math.trunc(15.17))
